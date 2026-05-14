b224fae3e020cfc0ee6f230a0b0984e75b3dc6a8
BejatTijana <bejatt.2004@gmail.com>
2026-05-14 12:09:28 +0200

    Dodana WeatherSample klasa

---
+++ Common/WeatherSample.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class WeatherSample
+    {
+        [DataMember] public double T { get; set; }
+        [DataMember] public double Pressure { get; set; }
+        [DataMember] public double Tpot { get; set; }
+        [DataMember] public double Tdew { get; set; }
+        [DataMember] public double VPmax { get; set; }
+        [DataMember] public double VPdef { get; set; }
+        [DataMember] public double VPact { get; set; }
+        [DataMember] public string Date { get; set; }
+    }
+}
