0ca19534114862e0374b92c4b13e8ff0903c7dc5
BejatTijana <bejatt.2004@gmail.com>
2026-05-14 12:31:36 +0200

    Dodan IWeatherService interfejs

---
+++ Common/IWeatherService.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IWeatherService
+    {
+        [OperationContract]
+        [FaultContract(typeof(DataFormatFault))]
+        string StartSession(SessionMeta meta);
+
+        [OperationContract]
+        [FaultContract(typeof(ValidationFault))]
+        string PushSample(WeatherSample sample);
+
+        [OperationContract]
+        string EndSession();
+    }
+}
