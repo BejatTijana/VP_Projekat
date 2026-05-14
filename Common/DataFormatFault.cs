ed6234c852dbecacc0e136b0705564396ea2fee8
n2004ina <ninaabudjen@gmail.com>
2026-05-14 12:21:22 +0200

    Dodane SessionMeta, DataFormatFault i ValidationFault klase

---
+++ Common/DataFormatFault.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class DataFormatFault
+    {
+        [DataMember] public string Message { get; set; }
+        public DataFormatFault(string message) { Message = message; }
+    }
+}
---
+++ Common/SessionMeta.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class SessionMeta
+    {
+        [DataMember] public string StationName { get; set; }
+        [DataMember] public string Description { get; set; }
+    }
+}
