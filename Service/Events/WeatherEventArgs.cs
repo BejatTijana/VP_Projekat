6e86b59a745a6bb7488419acafc34a1fcb6009ca
n2004ina <ninaabudjen@gmail.com>
2026-05-14 12:42:51 +0200

    Dodan WeatherAnalyzer sa detekcijom anomalija

--- Service/Events/WeatherEventArgs.cs
+++ Service/Events/WeatherEventArgs.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Common;
 
 namespace Service.Events
 {
-    internal class WeatherEventArgs
+    public class SampleReceivedEventArgs : EventArgs
     {
+        public WeatherSample Sample { get; }
+        public SampleReceivedEventArgs(WeatherSample sample) { Sample = sample; }
+    }
+
+    public class WarningEventArgs : EventArgs
+    {
+        public string ParameterName { get; }
+        public string Direction { get; }
+        public double Value { get; }
+        public WarningEventArgs(string paramName, string direction, double value)
+        {
+            ParameterName = paramName;
+            Direction = direction;
+            Value = value;
+        }
+    }
+
+    public class TransferEventArgs : EventArgs
+    {
+        public string SessionName { get; }
+        public TransferEventArgs(string sessionName) { SessionName = sessionName; }
     }
 }
---
+++ Service/WeatherAnalyzer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Configuration;
+using Common;
+using Service.Events;
+
+namespace Service
+{
+    public class WeatherAnalyzer
+    {
+        private double P_threshold;
+        private double VPact_threshold;
+        private double VPdef_threshold;
+
+        private double pressureSum = 0;
+        private int pressureCount = 0;
+        private WeatherSample previous = null;
+
+        public delegate void WarningHandler(object sender, WarningEventArgs e);
+        public event WarningHandler OnWarningRaised;
+
+        public WeatherAnalyzer()
+        {
+            P_threshold = double.Parse(ConfigurationManager.AppSettings["P_threshold"]);
+            VPact_threshold = double.Parse(ConfigurationManager.AppSettings["VPact_threshold"]);
+            VPdef_threshold = double.Parse(ConfigurationManager.AppSettings["VPdef_threshold"]);
+        }
+
+        public void Analyze(WeatherSample current)
+        {
+            if (previous == null)
+            {
+                previous = current;
+                pressureSum += current.Pressure;
+                pressureCount++;
+                return;
+            }
+
+            CheckPressureSpike(current);
+            CheckVPActSpike(current);
+            CheckVPDefSpike(current);
+
+            pressureSum += current.Pressure;
+            pressureCount++;
+            previous = current;
+        }
+
+        private void CheckPressureSpike(WeatherSample current)
+        {
+            double deltaP = current.Pressure - previous.Pressure;
+            if (Math.Abs(deltaP) > P_threshold)
+            {
+                string direction = deltaP > 0 ? "iznad" : "ispod";
+                OnWarningRaised?.Invoke(this, new WarningEventArgs("PressureSpike", direction, current.Pressure));
+            }
+
+            double mean = pressureSum / pressureCount;
+            if (current.Pressure < 0.75 * mean || current.Pressure > 1.25 * mean)
+            {
+                string direction = current.Pressure > mean ? "iznad" : "ispod";
+                OnWarningRaised?.Invoke(this, new WarningEventArgs("OutOfBandWarning (Pressure)", direction, current.Pressure));
+            }
+        }
+
+        private void CheckVPActSpike(WeatherSample current)
+        {
+            double delta = current.VPact - previous.VPact;
+            if (Math.Abs(delta) > VPact_threshold)
+            {
+                string direction = delta > 0 ? "iznad" : "ispod";
+                OnWarningRaised?.Invoke(this, new WarningEventArgs("VPActSpike", direction, current.VPact));
+            }
+        }
+
+        private void CheckVPDefSpike(WeatherSample current)
+        {
+            double delta = current.VPdef - previous.VPdef;
+            if (Math.Abs(delta) > VPdef_threshold)
+            {
+                string direction = delta > 0 ? "iznad" : "ispod";
+                OnWarningRaised?.Invoke(this, new WarningEventArgs("VPDefSpike", direction, current.VPdef));
+            }
+        }
+    }
+}
